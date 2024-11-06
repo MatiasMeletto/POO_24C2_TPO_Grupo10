d8a6d1c53e728d8021b549d9d9561cdc7ce41ed8
Franco2444 <116115512+Franco2444@users.noreply.github.com>
2024-11-06 22:53:21 +0000

    Add files via upload

---
+++ Reino V3/modelo/ObjetoPerdido.java
@@ -0,0 +1,17 @@
+public class ObjetoPerdido extends MisionSecundaria {
+    private String nombreObjeto;
+
+    public ObjetoPerdido(Ubicacion ubicacion, int recompensa, String nombreObjeto) {
+        super(ubicacion, recompensa);
+        this.nombreObjeto = nombreObjeto;
+    }
+
+    public boolean verificarCompletada() {
+        // Implementa la lógica para verificar si el objeto ha sido recuperado
+        return true; // Cambia esto según la lógica de juego
+    }
+
+    public String getNombreObjeto() {
+        return nombreObjeto;
+    }
+}
